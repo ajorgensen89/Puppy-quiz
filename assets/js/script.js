f7aeb6aa024e79e57b34edac005da162f73a7570
ajorgensen89 <ajorgensen89@gmail.com>
2022-12-24 11:43:31 +0000

    Add Question/answer arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,8 @@ document.addEventListener("DOMContentLoaded", function () {
          * runs the code in this block
          */
         button.addEventListener("click", function () {
-            /**refers to specific data-type button that has been clicked */
+            /**refers to specific data-type button that has been clicked
+             */
             if (this.getAttribute("data-type") === "submit") {
                 alert("clicked submit");
             } else {
@@ -16,8 +17,119 @@ document.addEventListener("DOMContentLoaded", function () {
         })
     }
 })
-
+/**The main game which is be first loaded and then again
+ * after the user answer is processed
+*/
 function runQuiz() {
+    /**List of questions to run in game with question, answer, choice and isCorrect lists in array */
+let numberOfQuestions = [{
+
+        question: "What is naughty Majors name in Paw Patrol who causes problems for the Paw Patrol gang?",
+        answers: [{
+                choice: "Major Goodway",
+                isCorrect: false
+            },
+            {
+                choice: "Major Humdinger",
+                isCorrect: true
+            },
+            {
+                choice: "Major Johnson",
+                isCorrect: false
+            },
+        ]
+
+    },
+    {
+
+        question: "What name is given to the puppy who wears a blue police unifrom in Paw Patrol?",
+        answers: [{
+                choice: "Chase",
+                isCorrect: true
+            },
+            {
+                choice: "Rumble",
+                isCorrect: false
+            },
+            {
+                choice: "Tracker",
+                isCorrect: false
+            },
+        ],
+
+    },
+    {
+
+        question: "What is the name of Major Goodways pet chicken?",
+        answers: [{
+                choice: "Wally",
+                isCorrect: false
+            },
+            {
+                choice: "Chickaletta",
+                isCorrect: true
+            },
+            {
+                choice: "Ryder",
+                isCorrect: false
+            },
+        ]
+
+    },
+    {
+
+        question: "What name is given to the puppy who wears a blue police unifrom?",
+        answers: [{
+                choice: "Chase",
+                isCorrect: true
+            },
+            {
+                choice: "Rumble",
+                isCorrect: false
+            },
+            {
+                choice: "Tracker",
+                isCorrect: false
+            },
+        ]
+
+    },
+    {
+        question: "Where do the Paw Patrol pups live with Ryder?",
+        answers: [{
+                choice: "Tinsel Town",
+                isCorrect: false
+            },
+            {
+                choice: "Lands End",
+                isCorrect: false
+            },
+            {
+                choice: "Adventure Bay",
+                isCorrect: true
+            },
+        ]
+
+    }, {
+        question: "What name is given to the puppy who wears a pink helicopter pilot uniform?",
+        answers: [{
+                choice: "Rocky",
+                isCorrect: false
+            },
+            {
+                choice: "Everest",
+                isCorrect: false
+            },
+            {
+                choice: "Sky",
+                isCorrect: true
+            },
+        ]
+
+    },
+];
+
+
 
 }
 
