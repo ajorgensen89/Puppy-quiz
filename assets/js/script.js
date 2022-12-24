6244336770fbd36557077b6b5f2a60cdb7b1e31d
ajorgensen89 <ajorgensen89@gmail.com>
2022-12-24 10:59:01 +0000

    Add functions to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
-console.log("check");
+function runQuiz() {
+
+}
+function checkAnswer() {
+
+}
+function calculateAnswer() {
+
+}
+function increaseScore() {
+
+}
+function increaseIncorrect() {
+
+}
+function displayAnswer1() {
+
+}
+function displayAnswer2() {
+    
+}
+function displayAnswer3() {
+    
+}
+function displayQuestion() {
+    
+}
